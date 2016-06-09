d3054516cad774e47a8921a2f5fc391b721f5179
Francesco Delle Fave <francesco.dellefave@gmail.com>
2016-06-09 15:09:24 -0400

    new libraries

--- Python/parsing_utils/parsing_utils.py
+++ Python/parsing_utils/parsing_utils.py
@@ -1,18 +1,23 @@
 import pandas as pd
 from csv import DictReader
-from collections import DefaultDict, Counter
+from collections import defaultdict, Counter
 from nltk.tokenize import RegexpTokenizer
 import re
+import sys
+#import operator
 
 file_path = "../datasets/StanceDataset/"
+
 class ParsingUtils:
     def initialize(self, file_name):
         self.file_name = file_path + file_name
-        self.count = Counter()
+        self.rates = defaultdict(float)
         self.rare_tweets_counter()
         self.threshold = 5.0
 
     def rare_tweets_counter(self):
+        count = Counter()
+        self.rates = defaultdict(float)
         with open(self.file_name, 'rU') as f:
             reader = DictReader(f)
             for row in reader:
@@ -21,13 +26,11 @@ class ParsingUtils:
                     ls = s.split(' ')
                     for i in range( len(ls) ):
                         if '@' in ls[i]:
-                            self.count[ ls[i] ] += 1
+                            count[ ls[i] ] += 1
             f.close()
-        total = sum(self.count.values())
-        rates = DefaultDict(float)
-        for k in self.count.keys():
-            rates[ k ] = 100 * ( self.count[k] / float(total) )
-        return rates
+        total = sum(count.values())
+        for k in count.keys():
+            self.rates[ k ] = 100 * ( count[k] / float(total) )
 
     def purify(self, tweet):
         ts = str(tweet).lower()
@@ -36,7 +39,7 @@ class ParsingUtils:
             if 'http' in ts_l or 'www' in ts_l:
                 ts_l[i] = 'HYPERLINK'
             if '@' in ts_l[i]:
-                rare = self.count[ ts_l[i] ] < self.threshold
+                rare = self.rates[ ts_l[i] ] < self.threshold
                 if rare:
                     ts_l[i] = 'RARE_USER'
                 else: 
@@ -49,8 +52,8 @@ class ParsingUtils:
         ts_t = tokenizer.tokenize( ' '.join(ts_l) )
         return ' '.join(ts_t)
 
-    def create_dataFrame(self):
-        data_dict = DefaultDict(list)
+    def create_dataFrame(self, target=None):
+        data_dict = defaultdict(list)
         names = []
         with open(self.file_name, 'rU') as f:
             reader = DictReader(f)
@@ -63,5 +66,31 @@ class ParsingUtils:
                         continue
                     data_dict[ n ].append( row[ n ] )
             f.close()
-            data_df = pd.DataFrame(data_dict,)
+            data_df = pd.DataFrame(data_dict, index=None)
+            if target:
+                cond = data_df[ 'Target' ] == target
+                data_df = data_df[ cond ]
         return data_df
+
+sys.stdout.write("Starting Parsing Utility")
+myUtil = ParsingUtils()
+sys.stdout.write("-> complete\n")
+sys.stdout.write("- Initializing Training Set")
+myUtil.initialize('train.csv')
+sys.stdout.write(" -> complete\n")
+sys.stdout.write("- Generating DataFrame")
+df = myUtil.create_dataFrame('Hillary Clinton')
+sys.stdout.write("-> complete\n")
+sys.stdout.write("- Saving to file")
+df.to_csv('../datasets/StanceDataset/train_purified.csv',index=False)
+sys.stdout.write("-> complete\n")
+
+sys.stdout.write("- Initializing Training Set")
+myUtil.initialize('test.csv')
+sys.stdout.write(" -> complete\n")
+sys.stdout.write("- Generating DataFrame")
+df = myUtil.create_dataFrame('Hillary Clinton')
+sys.stdout.write("-> complete\n")
+sys.stdout.write("- Saving to file")
+df.to_csv('../datasets/StanceDataset/test_purified.csv',index=False)
+sys.stdout.write("-> complete\n")
---
+++ Python/parsing_utils/w2v_generator.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+import logging
+import sys
+from gensim.models.word2vec import Word2Vec
+from gensim.models.phrases import Phrases
+from collections import defaultdict
+
+files_path = "../datasets/StanceDataset/"
+extension = ".csv"
+
+class W2V_Generator: 
+    def __init__(self, name):
+        self.name = name
+        self.wordAverages = defaultdict(list)
+        self.df = pd.read_csv(files_path + name + extension, header=0)
+        self.w2v_size = 300
+
+    def calcW2Vrep(self, log):
+        tweets = self.df.loc[:,'Tweet']
+        t_list = []
+        for t in tweets:
+            tl = t.split(' ')
+            for i in range( len( tl ) ):
+                tl[i] = tl[i].decode(encoding='utf-8', errors='ignore')
+            t_list.append( tl )
+        unigrams = Phrases(t_list, threshold=5.0, min_count=5)
+        if log:
+            logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
+            logging.info( "running %s" % " ".join(sys.argv) )
+    
+        tweets_unigrams = unigrams[ t_list ]
+        self.model = Word2Vec( tweets_unigrams, min_count=5, workers=12, size=self.w2v_size, window=5, negative=5, iter=50 )
+
+    def save_w2v_model(self):
+        self.model.save(files_path + self.name + ".doc2vec.word_model")
+        self.model.save_word2vec_format(files_path + self.name + ".doc2vec.word_vec")
+
+    def save_wordAvgs(self):
+        with open(files_path + self.name + ".wAvgs.tweets" + extension, 'wb') as tw:
+            tw.write("Tweet, Stance, Sentiment\n")
+            with open(files_path + self.name + ".wAvgs.values" + extension, 'wb') as tv:
+                for t, v in self.wordAverages.iteritems():
+                    try:
+                        row = self.df.loc[ self.df['Tweet'] == t ]
+                        stance = str( row['Stance'].values[0] )
+                        sentiment = str( row['Sentiment'].values[0] )
+                        tw.write(str(t) + "," + stance + "," + sentiment + "\n")                
+                        for i in range(v.size):
+                            tv.write( str(v[i]) )
+                            if i < v.size - 1: tv.write( "," )
+                            else: tv.write("\n")
+                    except KeyError:
+                        sys.stdout.write(str(idx) + ': ' + t)
+                        sys.stdout.write(" || NOT FOUND in the dataframe\n")
+                tv.close()
+            tw.close()
+
+    def convert_labels(self, task):
+        categories = {}
+        if task == 'Stance':
+            categories[ 'AGAINST' ] = -1
+            categories[ 'FAVOR' ] = 1
+            categories[ 'NONE' ] = 0
+        
+        if task == 'Sentiment':
+            categories[ 'neg' ] = -1
+            categories[ 'pos' ] = 1
+            categories[ 'other' ] = 0
+
+        for idx, row in self.df.iterrows():
+            row[ task ] = categories[ row[ task ] ]
+        
+    def calcWordAverages(self):
+        stop_words = [line.strip('\n') for line in open('../datasets/stop_words.txt','r')]
+        self.wordAvgs = defaultdict(np.ndarray)
+        for i, tweet in self.df['Tweet'].iteritems():
+            size = len( tweet )
+            tw_list = tweet.split(' ')
+            avg = np.zeros(self.w2v_size)
+            for word in tw_list:
+                if word in stop_words: continue
+                if str(word) in self.model.vocab:
+                    avg += self.model[ word ]
+            self.wordAverages[ tweet ] = avg / float (size)    
+
+######################################
+######################################
+######################################
+
+# TRAINING SET
+sys.stdout.write("1 - Training SET: set up W2V utilities: \n")
+trainW2V = W2V_Generator('train_purified')
+sys.stdout.write("-- Converting Sentiment / Stance to int")
+trainW2V.convert_labels('Sentiment')
+trainW2V.convert_labels('Stance')
+sys.stdout.write(" -> complete\n")
+sys.stdout.write("-- Generating / Saving W2V representation")
+trainW2V.calcW2Vrep(False)
+trainW2V.save_w2v_model()
+sys.stdout.write(" -> complete\n")
+sys.stdout.write("-- Calculating / Saving Tweet averages")
+trainW2V.calcWordAverages()
+trainW2V.save_wordAvgs()
+sys.stdout.write(" -> complete\n")
+
+# TEST SET
+sys.stdout.write("1 - Test SET: set up W2V utilities: \n")
+testW2V = W2V_Generator('test_purified')
+sys.stdout.write("-- Converting Sentiment / Stance to int")
+testW2V.convert_labels('Sentiment')
+testW2V.convert_labels('Stance')
+sys.stdout.write(" -> complete\n")
+sys.stdout.write("-- Generating / Saving W2V representation")
+testW2V.calcW2Vrep(False)
+testW2V.save_w2v_model()
+sys.stdout.write(" -> complete\n")
+sys.stdout.write("-- Calculating / Saving Tweet averages")
+testW2V.calcWordAverages()
+testW2V.save_wordAvgs()
+sys.stdout.write(" -> complete\n")
